b8d271a4a7c062d8ff977e90a201c3b65ca67eb9
Naland <naland.techwise@gmail.com>
2024-04-03 18:27:28 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './account/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PopupModule } from '@progress/kendo-angular-popup';
+import { SharedModule } from '@progress/kendo-angular-inputs';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { UserService } from './services/user.service';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule ,
+    PopupModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule       
+    
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
